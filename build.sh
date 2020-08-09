99e29ecd8f191edbd6c7e98589e78f036c141a84
itswege <pratamaxwg@gmail.com>
2020-08-09 11:07:52 +0700

    fix nama yg salah xD

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #Deleting Kernel Image Before
 cd /home/itswege/itswege/Source
 echo "Deleting Kernel Image Before"
-rm -rf ame
+rm -rf kata
 
 # Main Environment
 export ARCH=arm64
@@ -13,9 +13,9 @@ export KBUILD_BUILD_HOST="wege-Local"
 export CROSS_COMPILE=/home/itswege/itswege/gcc64/aarch64-linux-gnu/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=/home/itswege/itswege/gcc32/aarch64-linux-gnu/bin/arm-linux-androideabi-
 
-make O=ame ARCH=arm64 SUBARCH=arm64 X01BD_defconfig
+make O=kata ARCH=arm64 SUBARCH=arm64 X01BD_defconfig
 
-make -j$(nproc --all) O=ame ARCH=arm64 \
+make -j$(nproc --all) O=kata ARCH=arm64 \
                         CC="/home/itswege/itswege/proton/bin/clang" \
                         CLANG_TRIPLE="aarch64-linux-gnu-"
 
