bfa009a1bae54a869d3c396f689774dd819f76e9
wegee <pratamaxwg@gmail.com>
2020-08-09 11:36:25 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,12 +10,12 @@ export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER="itswege"
 export KBUILD_BUILD_HOST="wege-Local"
-export CROSS_COMPILE=/home/itswege/itswege/gcc64/aarch64-linux-gnu/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=/home/itswege/itswege/gcc32/aarch64-linux-gnu/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/home/itswege/itswege/Toolchain/gcc64/aarch64-linux-gnu/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/home/itswege/itswege/Toolchain/gcc32/aarch64-linux-gnu/bin/arm-linux-androideabi-
 
 make O=kata ARCH=arm64 SUBARCH=arm64 X01BD_defconfig
 
 make -j$(nproc --all) O=kata ARCH=arm64 \
-                        CC="/home/itswege/itswege/proton/bin/clang" \
+                        CC="/home/itswege/itswege/Toolchain/proton/bin/clang" \
                         CLANG_TRIPLE="aarch64-linux-gnu-"
 
