4a035383d305c3319976a9a40cf8d64e6398553b
wegee <pratamaxwg@gmail.com>
2020-08-23 01:10:44 +0700

    CircleCI: langsung gasken build 4 kernel :v

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,7 +3,7 @@ echo "Cloning dependencies"
 git clone --depth=1 https://github.com/itswege/fiency_kernel -b nomi nomi
 cd nomi
 git clone --depth=1 https://github.com/ZyCromerZ/DragonTC -b daily/10.0 clang
-git clone --depth=1 https://github.com/itswege/AnyKernel3-1 AnyKernel
+git clone --depth=1 https://github.com/itswege/AnyKernel3-1 -b lavender AnyKernel
 git clone --depth=1 https://github.com/najahiiii/aarch64-linux-gnu.git -b 4.9-mirror gcc64
 git clone --depth=1 https://github.com/najahiiii/aarch64-linux-gnu.git -b 4.9-32-mirror gcc32
 echo "Done"
@@ -32,7 +32,7 @@ cp nomi/arch/arm64/boot/Image.gz-dtb AnyKernel
 # Zipping
 function zipping1() {
     cd AnyKernel || exit 1
-    zip -r9 [${TANGGAL}]Fiency-noMi.zip *
+    zip -r9 [${TANGGAL}]noMi-60Hz.zip *
     cd ..
 }
 
@@ -57,6 +57,7 @@ cd ..
 echo "Cloning dependencies"
 git clone --depth=1 https://github.com/itswege/fiency_kernel -b  ameno ame
 cd ame
+git clone --depth=1 https://github.com/itswege/AnyKernel3-1 -b ame AnyKernel
 echo "Done"
 TANGGAL=$(date +"%d%m" )
 START=$(date +"%s")
@@ -107,6 +108,7 @@ cd ..
 echo "Cloning dependencies"
 git clone --depth=1 https://github.com/itswege/fiency_kernel -b  ame65 ame65
 cd ame65
+git clone --depth=1 https://github.com/itswege/AnyKernel3-1 -b ame65 AnyKernel
 echo "Done"
 TANGGAL=$(date +"%d%m" )
 START=$(date +"%s")
@@ -151,3 +153,52 @@ END=$(date +"%s")
 DIFF=$(($END - $START))
 push3
 
+# Kata
+cd ..
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/itswege/fiency_kernel -b kata kata
+cd kata
+git clone --depth=1 https://github.com/itswege/AnyKernel3-1 -b kata AnyKernel
+echo "Done"
+TANGGAL=$(date +"%d%m" )
+START=$(date +"%s")
+# Build kata
+echo "Starting building Katamari"
+
+# Main Environment
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER="itswege"
+export KBUILD_BUILD_HOST="circleci"
+export CROSS_COMPILE="/root/project/fiency/nomi/gcc64/bin/aarch64-linux-android-"
+export CROSS_COMPILE_ARM32="/root/project/fiency/nomi/gcc32/bin/arm-linux-androideabi-"
+
+make O=nomi ARCH=arm64 SUBARCH=arm64 X01BD_defconfig
+
+make -j$(nproc --all) O=nomi ARCH=arm64 \
+                        CC="/root/project/fiency/nomi/clang/bin/clang" \
+                        CLANG_TRIPLE="aarch64-linux-gnu-"
+                        
+cp nomi/arch/arm64/boot/Image.gz-dtb AnyKernel
+
+# Zipping
+function zipping4() {
+    cd AnyKernel || exit 1
+    zip -r9 [${TANGGAL}]KatamaRi-60Hz.zip *
+    cd ..
+}
+
+# Push kernel to Tele
+function push4() {
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot1297366089:AAHXfN5kK3BKWyVOk3JmH7Aa8HNbIpnJKhE/sendDocument" \
+        -F chat_id="-1001431143583" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)."
+}
+zipping4
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push4
