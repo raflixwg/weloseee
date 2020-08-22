e02d6992b41a3fb4f803a5df9ff5a7df908b4313
itswege <pratamaxwg@gmail.com>
2020-08-22 12:27:06 +0700

    bucin.sh: add solo builder
    Signed-off-by: itswege <pratamaxwg@gmail.com>

---
+++ bucin.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Deleting Kernel Image Before
+cd /home/itswege/itswege/Ame
+echo "Deleting Kernel Image Before"
+rm -rf ame
+
+# Main Environment
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER="itswege"
+export KBUILD_BUILD_HOST="wege-Local"
+export CROSS_COMPILE=/home/itswege/itswege/Toolchain/gcc64/aarch64-linux-gnu/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/home/itswege/itswege/Toolchain/gcc32/aarch64-linux-gnu/bin/arm-linux-androideabi-
+
+make O=ame ARCH=arm64 SUBARCH=arm64 bucin_defconfig
+
+make -j$(nproc --all) O=ame ARCH=arm64 \
+                        CC="/home/itswege/itswege/Toolchain/dtc/DragonTC/bin/clang" \
+                        CLANG_TRIPLE="aarch64-linux-gnu-"
+
