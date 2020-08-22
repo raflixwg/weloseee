512d562bd08b7d71ec33dc732d68eab6ce781ebe
itswege <pratamaxwg@gmail.com>
2020-08-22 13:05:55 +0700

    bucin.sh: fix error defconfig
    Signed-off-by: itswege <pratamaxwg@gmail.com>

--- bucin.sh
+++ bucin.sh
@@ -13,7 +13,7 @@ export KBUILD_BUILD_HOST="wege-Local"
 export CROSS_COMPILE=/home/itswege/itswege/Toolchain/gcc64/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=/home/itswege/itswege/Toolchain/gcc32/bin/arm-linux-androideabi-
 
-make O=kata ARCH=arm64 SUBARCH=arm64 bucin_defconfig
+make O=kata ARCH=arm64 SUBARCH=arm64 X01BD_defconfig
 
 make -j$(nproc --all) O=kata ARCH=arm64 \
                         CC="/home/itswege/itswege/Toolchain/dtc/DragonTC/bin/clang" \
