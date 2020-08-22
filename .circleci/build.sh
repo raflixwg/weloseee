90ed0cc23cd586cc53074cb8326c878ed295b847
wegee <pratamaxwg@gmail.com>
2020-08-23 00:27:17 +0700

    eol

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -44,7 +44,7 @@ function push() {
         -F chat_id="-1001450556997" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>XoBoD</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)."
 }
 zipping
 END=$(date +"%s")
